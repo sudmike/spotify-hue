aae4ffcf5f504773a5a15106344f6aabfce8db96
sudmike <mike.sudhaus@gmail.com>
2020-10-22 22:32:05 +0200

    Manually Added a Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
+/* Import Components */
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ /* Register Components */
+    AppComponent,
+    ServerComponent
   ],
     imports: [
         BrowserModule,
