59230b7fca8c2d9e205765abd4416a6e4536cb82
sudmike <mike.sudhaus@gmail.com>
2021-02-09 21:44:55 +0100

    spotify access tokens automatically get refreshed

--- src/app/services/spotify-web.service.ts
+++ src/app/services/spotify-web.service.ts
@@ -10,52 +10,73 @@ import { Track } from '../shared/track.model';
 export class SpotifyWebService {
 
   private spotifyApi = new SpotifyWebApi();
+  private spotifyTokenTime: number;
 
   constructor( private backendService: BackendCommsService) { }
 
   setAccessToken(accessToken: string): void {
     this.spotifyApi.setAccessToken(accessToken);
 
+    this.spotifyTokenTime = Math.round(Date.now() / 1000);
+
     console.log('Access Token Set');
   }
 
   async getCurrentTrack(): Promise<{track: Track, remaining_ms: number}> {
 
-    return this.spotifyApi.getMyCurrentPlayingTrack()
+    if (Math.round(Date.now() / 1000) > this.spotifyTokenTime + (60 * 55)) { // access token should be renewed
+      return this.refreshAccessToken()
+        .then(() => {
+          return this.getCurrentTrack();
+        })
+        .catch(err => {
+          return Promise.reject(err);
+        });
+    }
+
+    else { // access token is still valid
+      return this.spotifyApi.getMyCurrentPlayingTrack()
+        .then(data => {
+          if (!data.item){ // case that Spotify answers but without any data
+            return Promise.reject(Error('Not currently listening'));
+          }
+          else {
+            return {
+              track: new Track(
+                data.item.id,
+                data.item.name,
+                data.item.artists.map(artist => artist.name),
+                data.item.album.images.map(image => image.url),
+                data.is_playing),
+              remaining_ms: data.item.duration_ms - data.progress_ms
+            };
+          }
+        })
+        .catch(err => {
+          if (err instanceof Error){
+            return Promise.reject(err);
+          }
+          else {
+            return Promise.reject(Error('Unable to get current Track'));
+          }
+        });
+    }
+  }
+
+  async refreshAccessToken(): Promise<void> {
+    return this.backendService.getSpotifyRefresh()
       .then(data => {
-        if (!data.item){ // case that Spotify answers but without any data
-          return Promise.reject(Error('Not currently listening'));
-        }
-        else{
-          return {
-            track: new Track(
-              data.item.id,
-              data.item.name,
-              data.item.artists.map(artist => artist.name),
-              data.item.album.images.map(image => image.url),
-              data.is_playing),
-            remaining_ms: data.item.duration_ms - data.progress_ms
-          };
-        }
+        this.setAccessToken(data.accessToken);
+        return Promise.resolve();
       })
       .catch(err => {
+        console.log(err);
         if (err instanceof Error){
           return Promise.reject(err);
         }
         else {
-          // console.log(err);
-          return Promise.reject(Error('Unable to get current Track'));
+          return Promise.reject(Error('Could not refresh access token!'));
         }
-      });
-  }
-
-  refreshAccessToken(): void {
-    this.backendService.getSpotifyRefresh()
-      .then(data => {
-        this.setAccessToken(data.accessToken);
-      })
-      .catch(err => {
-        console.log(err);
       });
   }
 }
